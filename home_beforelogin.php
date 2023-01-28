4a5dc2e772841385a08aa1bdc4eea786dc01f5f2
Woojin Choi <choiwooj@iu.edu>
2023-01-28 16:25:43 -0500

    home_beforelogin page and style css

---
+++ home_beforelogin.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Ridea</title>
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700&display=swap" rel="stylesheet">
+    <!-- icons -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <!-- another icons -->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    
+    <!-- navbar -->
+    <header>
+        <nav>
+        <a href="#" class="logo"><i class="ri-car-line"></i><span>RIDEA</span></a>
+            <ul class="navbar">
+                <li><a href="#">Schedule a Ride</a></li>
+                <li><a href="#">My Rides</a></li>
+                <li><a href="#">Calendar</a></li>
+                <li><a href="#">Chat</a></li>
+            </ul>
+            <div class="main">
+                <a href="#" class="user"><i class="ri-user-fill"></i>Sign in</a>
+                <a href="#">Register</a>
+                <div class="bx bx-menu" id="menu-icon"></div>
+            </div>
+        </nav>
+
+        <div class="hometext">
+            <h2>Tired of Waiting for </h2>
+            <h2>an Overpriced Taxi?</h2>
+            <a href="#" class="btn">Get a Ride!</a>
+        </div>
+        
+    </header>
+
+      
+    <script type="text/javascript" src="js/script.js"></script>
+    
+</body>
+</html> 
