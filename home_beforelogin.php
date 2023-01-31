79fab873800dc6531d4310b8f83b02ddb59a6fb1
Woojin Choi <choiwooj@iu.edu>
2023-01-31 00:34:45 -0500

    home before-after-css / adding links to other php

--- home_beforelogin.php
+++ home_beforelogin.php
@@ -4,46 +4,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Ridea</title>
+    <title>RIDEA</title>
     <!-- google fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500;700&family=Roboto:wght@100;400;700&display=swap" rel="stylesheet">
     <!-- icons -->
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <!-- another icons -->
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 </head>
 <body>
     
     <!-- navbar -->
     <header>
         <nav>
+        <!-- ridea logo -->
         <a href="#" class="logo"><i class="ri-car-line"></i><span>RIDEA</span></a>
-            <ul class="navbar">
-                <li><a href="#">Schedule a Ride</a></li>
-                <li><a href="#">My Rides</a></li>
-                <li><a href="#">Calendar</a></li>
-                <li><a href="#">Chat</a></li>
-            </ul>
+        <!-- sign in and register buttons -->
             <div class="main">
-                <a href="#" class="user"><i class="ri-user-fill"></i>Sign in</a>
-                <a href="#">Register</a>
+                <a href="loginform.php" class="user"><i class="ri-user-fill"></i>Sign in</a>
+                <a href="register1.php">Register</a>
                 <div class="bx bx-menu" id="menu-icon"></div>
             </div>
         </nav>
-
+        <!-- main text in the home -->
         <div class="hometext">
-            <h2>Tired of Waiting for </h2>
-            <h2>an Overpriced Taxi?</h2>
-            <a href="#" class="btn">Get a Ride!</a>
+            <h2 class="slide-left">Tired of Waiting for <br> an Overpriced Taxi?<br></h2>
+            <a  href="loginform.php" class="btn"><span>Get a Ride!</span></a>
         </div>
         
+        <div class="bg-image">
+        </div>
     </header>
-
-      
-    <script type="text/javascript" src="js/script.js"></script>
     
 </body>
 </html> 
