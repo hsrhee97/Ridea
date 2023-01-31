c6938d6e5a5de1c3699a261145aa921dda25ee34
borat <borat@iu.edu>
2023-01-31 14:57:27 -0500

    User Survey

--- usersurvey.php
+++ usersurvey.php
@@ -11,6 +11,18 @@
 </head>
 <body>
     <div class="wrapper">
+    <header>
+        <nav>
+        <a href="#" class="logo"><i class="ri-car-line"></i><span>RIDEA</span></a>
+            <ul class="navbar">
+                <li><a href="#">Schedule a Ride</a></li>
+                <li><a href="#">My Rides</a></li>
+                <li><a href="#">Calendar</a></li>
+                <li><a href="#">Chat</a></li>
+            </ul>
+        </nav>        
+    </header>
+
         <h2>Ride Reservation</h2>
         <p>Please fill this form to book a ride.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
@@ -28,7 +40,7 @@
             </div>
             <div class="form-group">
                 <label>Date:</label>
-                <input type="date" name="date" class="form-control">
+                <input type="date" name="date" value="<?php echo date('Y-m-d'); ?>" class="form-control">
             </div>
             <div class="form-group">
                 <label>Other:</label>
@@ -38,7 +50,6 @@
                 <input type="submit" class="btn btn-primary" value="Submit">
                 <input type="reset" class="btn btn-secondary ml-2" value="Reset">
             </div>
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
         </form>
     </div>    
 </body>
