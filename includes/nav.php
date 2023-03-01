b8e7f66affc4f3e37164568e28c8b6499db22bd0
Woojin Choi <choiwooj@iu.edu>
2023-03-01 00:35:01 -0500

    navbar, narbar.css, style(home).css, php

--- includes/nav.php
+++ includes/nav.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <style> <?php include 'css/style.css'; ?> </style>
+    <style> <?php include 'css/navbar.css'; ?> </style>
 
     <title>RIDEA</title>
     <!-- google fonts -->
@@ -20,7 +20,6 @@
     <!-- another icons -->
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-    
 
 </head>
 
