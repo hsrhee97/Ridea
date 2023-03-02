db7cfe806a4f9ecaf634eeaef75cdf02e17690e5
rheeh <rheeh@iu.edu>
2023-03-02 13:50:52 -0500

    change comment

--- matching.php
+++ matching.php
@@ -194,8 +194,6 @@
 
             //None
             if (count($matches['different_all']) > 0) {
-                echo "There are no cases.";
-
                 echo "<p class='ask_book'>You can still proceed with the booking even if there are no matching results for your search criteria</p>";
 
                 echo "<a class='last_btn'href='calendar.php?TripID=".$row['TripID']."'>Book for future trip</a>";
@@ -203,6 +201,7 @@
 
 
         } else {
+            echo "<p class='ask_book'>There are no cases. You can still proceed with the booking even if there are no matching results for your search criteria</p>";
             echo "<a class='last_btn'href='calendar.php?TripID=".$row['TripID']."'>Book for future trip</a>";
         }
 
