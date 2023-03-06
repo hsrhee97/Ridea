b5577c787db9e30febda21cf34480e8c4f5f2e0a
Woojin Choi <choiwooj@iu.edu>
2023-03-06 01:50:13 -0500

    ride detail

--- ride-details.php
+++ ride-details.php
@@ -10,7 +10,6 @@
             if (!$conn) {
                 die("Failed to connect to MySQL: " . mysqli_connect_error());
             } else {
-                echo 'done';
             }
 
             $type = $_SESSION['type'];
@@ -27,20 +26,14 @@
             $p_name ='';
             $p_fname ='';
             $p_lname ='';
-
-            echo $type;
-            echo $email;
             
             if ($type =='driver') {
                 $TripID = $_GET['TripID'];
-                echo $TripID;
                 $sql = "SELECT * FROM DRIVER WHERE email='$email'";
                 $result = $conn->query($sql);
                 if ($result->num_rows > 0) {
                     while($row = $result->fetch_assoc()) {
                         $ID = $row["DriverID"];
-                    echo "id: " . $row["DriverID"]. " - Name: " 
-                        . $row["fname"]. " " . $row["lname"]. "<br>";
                     }
 
                 $passenger_id = "SELECT PassengerID FROM TRIP WHERE TripID = $TripID";
@@ -48,7 +41,6 @@
                 if ($result->num_rows > 0) {
                     while($row = $result->fetch_assoc()) {
                         $P_ID = $row["PassengerID"];
-                    echo "Passengerid: " . $row["PassengerID"]. "<br>";
                     }
 
                 $passenger_name = "SELECT * FROM PASSENGER WHERE PassengerID = '$P_ID'";
@@ -58,11 +50,7 @@
                         $p_name = $row["PassengerID"];
                         $p_fname = $row["fname"];
                         $p_lname = $row["lname"];
-                    echo "id: " . $row["PassengerID"]. " - Name: " 
-                        . $row["fname"]. " " . $row["lname"]. "<br>";
                     }
-                    echo $p_fname;
-                    echo $p_lname;
 
                 
                 $sql = "SELECT TripID, Start_location, End_location, Distance, Date FROM TRIP WHERE TripID = $TripID";
@@ -70,11 +58,9 @@
                 $num_rows = mysqli_num_rows($result);
             
                 if ($num_rows > 0) {
-                    echo "<table style='border:1px red; border-collapse: collapse; width:40%; border: solid 2px solid black;'>";
-                    echo "<tr style='border:1px solid black;'><th>TripID</th><th>Passenger First Name</th><th>Passenger Last Name</th><th>PassengerID</th><th>Start Location</th><th>End Location</th><th>Distance</th><th>Date</th>";
                 
                     while ($row = $result->fetch_assoc()) {
-                        echo "<tr style='border:1px solid black;'>";
+
                         echo "<td>" . $row["TripID"] . "</td>";
                         echo "<td>" . $p_fname . "</td>";
                         echo "<td>" . $p_lname . "</td>";
@@ -86,7 +72,7 @@
                         echo "<td>";
                         echo "</div>";
                         echo "</td>";
-                        echo "</tr>";
+
                     }
                     echo "</table>";
                 } else {
@@ -97,16 +83,13 @@
         }
     }
             elseif ($type == 'passenger'){
-                echo $ID;
                 $TripID = $_GET['TripID'];
-                echo $TripID;
                 $sql = "SELECT * FROM PASSENGER WHERE email='$email'";
                 $result = $conn->query($sql);
                 if ($result->num_rows > 0) {
                     while($row = $result->fetch_assoc()) {
                         $ID = $row["PassengerID"];
-                    echo "id: " . $row["PassengerID"]. " - Name: " 
-                        . $row["fname"]. " " . $row["lname"]. "<br>";
+
                     }
                     
                 $driver_id = "SELECT DriverID FROM TRIP WHERE TripID = $TripID";
@@ -114,7 +97,7 @@
                 if ($result->num_rows > 0) {
                     while($row = $result->fetch_assoc()) {
                         $D_ID = $row["DriverID"];
-                    echo "Driverid: " . $row["DriverID"]. "<br>";
+
                     }
                     
                 $driver_name = "SELECT * FROM DRIVER WHERE DriverID = '$D_ID'";
@@ -124,11 +107,8 @@
                         $d_name = $row["DriverID"];
                         $d_fname = $row["fname"];
                         $d_lname = $row["lname"];
-                    echo "id: " . $row["DriverID"]. " - Name: " 
-                        . $row["fname"]. " " . $row["lname"]. "<br>";
                     }
-                    echo $d_fname;
-                    echo $d_lname;
+
 
                 
 
